d750daddee207ed3a411ee265e43d62845ff9e75
jungmin <jungmin@sovoro.kr>
2024-08-27 09:58:59 +0900

    fate():summary

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import FreeBoards from './pages/FreeBoards';
 import FreeBoardDetail from './pages/FreeBoards/FreeBoardDetail';
 import FreeBoardCreate from './pages/FreeBoards/FreeBoardCreate';
+import SummaryBoard from '@/components/SummaryBoard';
 
 function App() {
   return (
     <Suspense fallback={<div style={{ fontSize: 200 }}>test</div>}>
       <Routes>
-        <Route path="/" element={<FreeBoards />} />
+        {/*<Route path="/" element={<FreeBoards />} />*/}
+        <Route path="/" element={<SummaryBoard />} />
         <Route path="create" element={<FreeBoardCreate />} />
         <Route path=":id" element={<FreeBoardDetail />} />
         <Route path=":id/:mode" element={<FreeBoardDetail />} />
